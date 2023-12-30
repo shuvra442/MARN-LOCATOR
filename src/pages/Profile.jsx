c239e9de0da195658257bd402b1302c31de3e268
shuvra442 <shuvrapatra5@gmail.com>
2023-12-30 14:41:18 +0530

    Update User functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,18 @@
 import React, { useRef, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
-import {app} from '../firebase';
-
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/User/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
@@ -11,44 +21,75 @@ export default function Profile() {
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
 
-
-
-useEffect (()=>{
-  if(file){
-    handleFileUpload(file);
-  }
-},[file]);
-
-const handleFileUpload = (file) =>{
-  const storage = getStorage(app);
-  const fileName = new Date().getTime() +file.name;
-  const storageRef = ref(storage,fileName);
-  const uploadTask = uploadBytesResumable(storageRef, file)
-  uploadTask.on('state_changed',
-  (snapshot)=>{
-    const progress = (snapshot.bytesTransferred/
-    snapshot.totalBytes)*100;
-    setFilePerc(Math.round(progress))
-  },
-  (error)=>{
-    setFileUploadError(true);
-  },
-  ()=>{
-    getDownloadURL(uploadTask.snapshot.ref).then
-    ((downloadURL) =>
-      setFormData({...formData, avatar: downloadURL})
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
     );
-  }
-  );
-};
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3ml font-semibold text-center my-7">Profile</h1>
 
-      <form className="flex flex-col gap-4">
-        <input onChange={(e)=> setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
           onClick={() => fileRef.current.click()}
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
@@ -56,33 +97,38 @@ const handleFileUpload = (file) =>{
           alt="profile-image"
         />
         <p className="text-sm self-center">
-          {
-            fileUploadError ?
-            (<span className="text-red-700">
+          {fileUploadError ? (
+            <span className="text-red-700">
               Error image upload (image must be less than 2 mb)
-            </span>) :filePerc > 0 && filePerc < 100 ? (
-              <span className="text-slate-700"> {`Uploading ${filePerc}%`}</span>
-            ) : filePerc === 100 ? (
-              <span className="text-green-700">Image successfully uploaded!</span>
-            ):(
-              ''
-            )}
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700"> {`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className="text-green-700">Image successfully uploaded!</span>
+          ) : (
+            ""
+          )}
         </p>
         <input
           type="text"
           placeholder="usrname"
+          defaultValue={currentUser.username}
           id="username"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
+          defaultValue={currentUser.email}
           id="email"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="password"
+          onChange={handleChange}
           id="password"
           className="border p-3 rounded-lg"
         />
